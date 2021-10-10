c3aa547314729285d3a2fb7fce1c1f71b20bbb8a
dhart <durellhart@live.com>
2021-10-10 18:17:44 -0400

    create hello world

---
+++ python_masterclass/hello_world.py
@@ -0,0 +1,9 @@
+print('Python is fun')
+print("Python's strings are easy to use")
+print('We can even include "quotes" in strings')
+print("hello"+"world")
+greetings = "Hello"
+name = input("Please enter your name ")
+
+# if we want a space, we can add that too
+print(greeting + '' + name)
