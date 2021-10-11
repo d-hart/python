a4e3175d52fbfa7260fc7aaa255b004ef845167e
dhart <durellhart@live.com>
2021-10-10 20:06:08 -0400

    Created a few practice files

--- python_masterclass/hello_world.py
+++ python_masterclass/hello_world.py
@@ -1,9 +1,1 @@
-print('Python is fun')
-print("Python's strings are easy to use")
-print('We can even include "quotes" in strings')
-print("hello"+"world")
-greetings = "Hello"
-name = input("Please enter your name ")
-
-# if we want a space, we can add that too
-print(greeting + '' + name)
+print("Hello, world")
---
+++ python_masterclass/strings.py
@@ -0,0 +1,20 @@
+print('Python is fun')
+print("Python's strings are easy to use")
+print('We can even include "quotes" in strings')
+print("hello"+"world")
+greeting = "Hello"
+name = "Rell"
+print(greeting + name)
+
+# if we want a space, we can add that too
+print(greeting + '' + name)
+
+age = 24
+print(age)
+
+print(type(greeting))
+print(type(age))
+
+age = "2 years"
+print(age)
+print(type(age))
