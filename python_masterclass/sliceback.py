1b3a8d28d5c7c3f08cf624c6a34a121e3b25ed0f
dhart <durellhart@live.com>
2021-10-23 14:14:55 -0400

    more python practice

---
+++ python_masterclass/sliceback.py
@@ -0,0 +1,13 @@
+letters = 'abcdefghijklmnopqrstuvwxyz'
+
+backwards = letters[25:0:-1]
+print(backwards)
+
+qpo = letters[-10:-13:-1]
+print(qpo)
+
+edcba = letters[4::-1]
+print(edcba)
+
+last_eight = letters[-1:-9:-1]
+print(last_eight)
